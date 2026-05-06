c4a90f679d903d6fe390133dc1e45c5009abeaa8
Fran <francastt03@gmail.com>
2026-05-06 16:26:36 +0200

    problemas con la zona horaria

--- backend/src/main/java/com/corty/backend/BackendApplication.java
+++ backend/src/main/java/com/corty/backend/BackendApplication.java
@@ -1,16 +1,24 @@
 package com.corty.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BackendApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
